5dd2bcc0672fe314b951de7d15e944409e23be46
jovove <jovan.velkoski@varvet.com>
2018-09-27 21:53:18 +0200

    Update 2

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,11 +1,39 @@
 // This is where it all goes :)
-window.onload = function () {
+window.onload = function() {
   lottie.loadAnimation({
-    container: document.getElementById("bm"),  // the dom element that will contain the animation
-    renderer: 'svg',
+    container: document.getElementById("bm"), // the dom element that will contain the animation
+    renderer: "svg",
     loop: true,
     autoplay: true,
-    path: '/images/qr-animation.json' // the path to the animation json
+    path: "/images/qr-animation.json" // the path to the animation json
   });
-  console.log("lottie animation loaded")
-}
+  console.log("lottie animation loaded");
+};
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+window.onload = function() {
+  btn.onclick = function() {
+    modal.style.display = "block";
+  };
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
